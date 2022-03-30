47535020b9db9bdad5549fe78eae7a060442a9bb
Linda <lindashen3@gmail.com>
2022-03-30 00:13:22 -0400

    Change render to createRoot method

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/sass/styles.css";
+// import { useState } from "react";
 function App() {
+  // on submit event from the form
+    // use the user query for the axios call
+    // use state as the param to the movie database API and goodreads API
+    // store the response in a state
+
+  // display information
+    // show movie poster and book cover when successful, otherwise ask user for new search query
+
+  // const [userInput, setUserInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Is the Book Better</h1>
+      <form action="#">
+        <input type="text" id="search" name="search" placeholder="Search" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
