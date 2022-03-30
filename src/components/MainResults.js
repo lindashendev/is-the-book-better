e9021e3b2d28d13da9b97ea7570c47c0c9e16332
Linda <lindashen3@gmail.com>
2022-03-30 16:54:22 -0400

    Use find method to locate matching object

--- src/components/MainResults.js
+++ src/components/MainResults.js
@@ -27,10 +27,30 @@ function MainResults({userQuery}) {
           }
         });
   
-    axios.all([movieRequest, googleBooksRequest]).then(axios.spread((res1, res2) => {
+    axios.all([movieRequest, googleBooksRequest]).then(axios.spread((movieData, booksData) => {
         setIsLoading(false);
-        console.log(res1);
-        console.log(res2);
+        // using response, if data matches, set State object 
+        // if no data found, alert trigger 
+          // if either are empty
+        // console.log(movieData.data.results);
+        // console.log(booksData.data.items);
+        const movieResults = movieData.data.results;
+        const booksResults = booksData.data.items
+        // loop through the arrays - of objects 
+          // movieData contains Object
+            // object.title
+            // object.vote_average
+          // booksData contains Object
+            // object.volumeInfo.title
+            // object.volumeInfo.averageRating
+        // loop find first item in each list that matches exactly, despite case used
+          // find separately if the search term exists 
+          // loop to get the term out of the first then find it in the second
+          // use the userQuery
+        const re = new RegExp(userQuery, 'i')
+        const found = movieResults.find(movie => {
+          return re.test(movie.title);
+        })
     }));
     } 
   }, [userQuery]);
